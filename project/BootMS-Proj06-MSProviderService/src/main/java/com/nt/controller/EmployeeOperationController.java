5e881e1ffa63338ca4e24737b7f1d7cb8129dee0
Satyam Sahoo <140689000+Satyam760@users.noreply.github.com>
2025-03-23 19:57:19 +0530

    Add files via upload

---
+++ project/BootMS-Proj06-MSProviderService/src/main/java/com/nt/controller/EmployeeOperationController.java
@@ -0,0 +1,23 @@
+package com.nt.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emp")
+public class EmployeeOperationController {
+
+	
+	@Value("${dbuser}")
+	private String user;
+	@Value("${dbpwd}")
+	private String pass;
+	
+	@GetMapping("/show")
+	public String displayData() {
+		return "Data collected through Config Server :: "+user+"..."+pass;
+	}
+	
+}
